fa2f77fa4f7f3c361330d23302ba8f5b673160cc
Cuthbert20 <knowltonspencer@gmail.com>
2020-02-19 16:30:46 -0700

    created logger middleware to console log the url and the HTTP Method used

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,11 +9,11 @@ const {
 const router = express.Router();
 
 router.get("/test", (req, res) => {
-  //   res.send("<h1>Test</h1>");
-  //   res.json({ name: "Spencer" });
+  //   res.send(`<h1>${req.hello}</h1>`);
+  res.json({ name: "Spencer" });
   //   res.sendStatus(400);
   //   res.status(400).json({ success: false });
-  res.status(200).json({ success: true, data: { id: 1 } });
+  //   res.status(200).json({ success: true, data: { id: 1 } });
 });
 
 //Routes Full CRUD
--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const bootcamps = require("./routes/bootcamps");
 //Load Enviroment Variables from our env file config.env
 dotenv.config({ path: "./config/config.env" });
 
+//Middleware
+
+const logger = (req, res, next) => {
+  const { method, originalUrl, protocol } = req;
+  console.log(`${method} ${protocol}://${req.get("host")}${originalUrl}`);
+  next();
+};
+app.use(logger);
 //Mount routes, middleware
 app.use("/api/v1/bootcamps", bootcamps);
 
