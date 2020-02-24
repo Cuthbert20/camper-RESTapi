676cbb0b4b88762ec883922a9dc9211f6e32bbad
Cuthbert20 <knowltonspencer@gmail.com>
2020-02-24 14:29:28 -0700

    fixing issue with 200 status on incorrect ids that had the correct format. Added conditional statment to make sure data/bootcamp exists

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -17,8 +17,21 @@ exports.getBootcamps = async (req, res, next) => {
 //@desc Get single bootcamp
 //@route GET /api/v1/bootcamps/:id
 //@access Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, mgs: `Get bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id); //any method after Bootcamp is a built in mongoose method.
+
+    if (!bootcamp) {
+      //we have to return because of multipule responses inside our try block.
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false, mgs: err });
+  }
+
+  //   res.status(200).json({ success: true, mgs: `Get bootcamp ${req.params.id}` });
 };
 
 //@desc Create new bootcamp
