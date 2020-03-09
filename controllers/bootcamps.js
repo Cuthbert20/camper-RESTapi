ff24b992c11812792ed0e30640d858aa3e16f3b4
Cuthbert20 <knowltonspencer@gmail.com>
2020-03-09 16:17:34 -0600

    setting up end point for getting bootcamp based on geocode

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,6 +2,7 @@ const Bootcamp = require("../models/Bootcamp");
 //Importing class from utils folder
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
 
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
@@ -86,8 +87,14 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   res
     .status(200)
     .json({ success: true, data: `${bootcamp.name} has been removed` });
+});
 
-  //   res
-  //     .status(200)
-  //     .json({ success: true, mgs: `Delete Bootcamp ${req.params.id}` });
+//@desc Get bootcamps within a global/radius using 2 parameters zipcode && distance (ie miles)
+//@route GET /api/v1/bootcamps/radius/:zipcode/:distance
+//@access Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  //destructuring params coming from url.
+  const { zipcode, distance } = req.params;
+
+  // Get latitude/longitude from geocoder
 });
