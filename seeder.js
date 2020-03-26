6a36b9d57d4df428087303fc6f922d8dc7284e02
Cuthbert20 <knowltonspencer@gmail.com>
2020-03-26 12:56:30 -0600

    modify seeder

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 
 //Load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,16 +19,20 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 });
 
-//Read JSON files
+//Read JSON files bootcamps.json
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+//Read JSON files courses.json
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
 //Import data into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-
+    await Course.create(courses);
     console.log("Data Imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -39,7 +44,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
-
+    await Course.deleteMany();
     console.log("Data Destroyed...".red.inverse);
     process.exit();
   } catch (err) {
